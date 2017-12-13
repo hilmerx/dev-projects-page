0980cd4e4e7dfb824989e1bd36df5b7e8618135b
hilmerx <joel.hilme@gmail.com>
2017-12-13 03:07:37 +0100

    projects in place, working version

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+document.getElementById("about").addEventListener("click", function(){
+    const modal = document.getElementsByClassName("modal")
+    modal[0].classList.toggle('modal--is-active');
+})
+
+document.getElementById("modal-background").addEventListener("click", function(){
+    const modal = document.getElementsByClassName("modal")
+    modal[0].classList.toggle('modal--is-active');
+})
+
+document.getElementById("modal-box").addEventListener("click", function(e){
+        e.stopPropagation();
+})
+
+document.getElementById("modal-x").addEventListener("click", function(e){
+    const modal = document.getElementsByClassName("modal")
+    modal[0].classList.toggle('modal--is-active');
+})
+
+$(window).scroll(function() {
+    var header = $(document).scrollTop();
+    console.log(header);
+
+    if (header > 120) {
+      $('.header').addClass('header--sticky-is-active');
+      $('.feed').addClass('feed--sticky-is-active');
+    } else {
+      $('.header').removeClass('header--sticky-is-active');
+      $('.feed').removeClass('feed--sticky-is-active');
+
+    }
+  });
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,52 @@
+let gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create(),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba')
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false,
+    open: false
+  })
+})
+
+// watch function
+
+gulp.task('watch', ['browserSync'], () => {
+
+  watch('./app/css/**/*.css', () => { gulp.start('cssInject') })
+  watch('./app/js/**/*.js', () => {
+    browserSync.reload()
+  } )
+  watch('./app/index.html', browserSync.reload )
+
+})
+
+
+
+// postcss function
+gulp.task('styles', () => {
+  return gulp.src('./app/css/main.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]).on('error', function(err) {// Error handling / not chrash the gulp watch
+            console.error(err.message);// Display error in the terminal
+            browserSync.notify(err.message, 5000); // Display error in the browser
+            this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
+        }))
+    .pipe(gulp.dest('./app/temp/css'))
+})
+
+
+// cssInject function
+gulp.task('cssInject', ['styles'], () => {
+  return gulp.src('./app/css/*.css')
+    .pipe(browserSync.stream())
+})
