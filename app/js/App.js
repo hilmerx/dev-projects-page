f01adc4fd55cf09cc27997e4da27c36cf6e4fb99
hilmerx <joel.hilme@gmail.com>
2017-12-18 19:19:44 +0100

    Changed copy

--- app/js/App.js
+++ app/js/App.js
@@ -1,8 +1,6 @@
 import Modal from './modules/Modal.js'
 import ScrollEvents from './modules/ScrollEvents.js'
-import {runGoogleAnalytics} from './modules/GoogleAnalytics.js'
 
 let modal = new Modal()
 let scrollEvents = new ScrollEvents()
 
-runGoogleAnalytics()
